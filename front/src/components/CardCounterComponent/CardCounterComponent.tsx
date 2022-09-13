418f35436f17fa66e1071a2aef7a7be6665003e6
BoltKun5 <fabdelkrim2000@gmail.com>
2022-09-13 14:07:01 +0200

    Added notification on quantity change

--- front/src/components/CardCounterComponent/CardCounterComponent.tsx
+++ front/src/components/CardCounterComponent/CardCounterComponent.tsx
@@ -1,16 +1,18 @@
-import React, {useContext, useEffect, useState} from "react";
-import {CardCounterComponentPropsType} from "../../../typing/types";
-import {loggedApi} from "../../axios";
+import React, { useContext, useEffect, useState } from "react";
+import { CardCounterComponentPropsType } from "../../../typing/types";
+import { loggedApi } from "../../axios";
 import CardManagerContext from "../../hook/contexts/CardManagerContext";
 import './CardCounterComponent.scss';
-import {Card} from "../../../../api/src/database";
+import { Card } from "../../../../api/src/database";
 
 export const CardCounterComponent: React.FC<CardCounterComponentPropsType> = ({
-                                                                                card, label, type,
-                                                                              }) => {
-  const {setCards, cards} = useContext(CardManagerContext);
+  card, label, type,
+}) => {
+  const { setCards, cards } = useContext(CardManagerContext);
   const [isDisabled, setIsDisabled] = useState<boolean>(false);
   const [value, setValue] = useState<string>("");
+  const [changeNotificationValue, setChangeNotificationValue] = useState<number | null>(null)
+  const [notifTimer, setNotifTimer] = useState<string | number | NodeJS.Timeout | undefined>(undefined);
 
   useEffect(() => {
     setValue(getValue(card))
@@ -18,23 +20,33 @@ export const CardCounterComponent: React.FC<CardCounterComponentPropsType> = ({
 
   const modifyQuantity = async (card: any, cardType: string, modification: string) => {
     setIsDisabled(true)
+    let oldValue;
+    let newValue;
     let classicQuantity = card?.userCardPossessions[0]?.classicQuantity ?? 0;
     let reverseQuantity = card?.userCardPossessions[0]?.reverseQuantity ?? 0;
     if (cardType === 'classic' && modification === 'plus') {
+      oldValue = classicQuantity;
       classicQuantity++;
+      newValue = classicQuantity;
     }
     if (cardType === 'classic' && modification === 'minus') {
+      oldValue = classicQuantity;
       classicQuantity--;
       if (classicQuantity < 0)
         classicQuantity = 0;
+      newValue = classicQuantity;
     }
     if (cardType === 'reverse' && modification === 'plus') {
+      oldValue = reverseQuantity;
       reverseQuantity++;
+      newValue = reverseQuantity;
     }
     if (cardType === 'reverse' && modification === 'minus') {
+      oldValue = reverseQuantity;
       reverseQuantity--;
       if (reverseQuantity < 0)
         reverseQuantity = 0;
+      newValue = reverseQuantity;
     }
 
     try {
@@ -50,9 +62,27 @@ export const CardCounterComponent: React.FC<CardCounterComponentPropsType> = ({
         }
         return localCard;
       }));
+      triggerChangeNotification(oldValue, newValue)
     } catch (e) {
       console.log(e)
     }
+  }
+
+
+  const startCountdown = () => {
+    clearTimeout(notifTimer);
+    setNotifTimer(setTimeout(() => {
+      setChangeNotificationValue(null);
+    }, 3000));
+  }
+
+  const triggerChangeNotification = async (oldValue: number, newValue: number) => {
+    if (changeNotificationValue !== null) {
+      setChangeNotificationValue(newValue - oldValue + changeNotificationValue)
+    } else {
+      setChangeNotificationValue(newValue - oldValue)
+    }
+    startCountdown();
   }
 
   const setQuantity = async (card: any, cardType: string, element: EventTarget & HTMLInputElement) => {
@@ -66,12 +96,14 @@ export const CardCounterComponent: React.FC<CardCounterComponentPropsType> = ({
       return
     }
     element.style.backgroundColor = 'white';
+    let oldValue;
 
     if (cardType === 'classic') {
       if (classicQuantity === Number(element.value)) {
         setIsDisabled(false)
         return
       }
+      oldValue = classicQuantity;
       classicQuantity = element.value;
     }
     if (cardType === 'reverse') {
@@ -79,6 +111,7 @@ export const CardCounterComponent: React.FC<CardCounterComponentPropsType> = ({
         setIsDisabled(false)
         return
       }
+      oldValue = reverseQuantity;
       reverseQuantity = element.value;
     }
 
@@ -89,6 +122,7 @@ export const CardCounterComponent: React.FC<CardCounterComponentPropsType> = ({
         reverseQuantity: reverseQuantity,
       });
       setIsDisabled(false)
+      triggerChangeNotification(Number(oldValue), Number(cardType === 'classic' ? classicQuantity : reverseQuantity))
       setCards(cards.map((localCard) => {
         if (card.id === localCard.id) {
           localCard.userCardPossessions[0] = response.data.data.result;
@@ -116,14 +150,16 @@ export const CardCounterComponent: React.FC<CardCounterComponentPropsType> = ({
       <div className="CardCounter-name">{label}</div>
       <div className="CardCounter-buttons">
         <button className="CardCounter-minus" disabled={isDisabled}
-                onClick={() => modifyQuantity(card, type, 'minus')}>-
+          onClick={() => modifyQuantity(card, type, 'minus')}>-
         </button>
         <input className="CardCounter-input" disabled={isDisabled}
-               onBlur={(ev) => setQuantity(card, type, ev.currentTarget)} onClick={(ev) => ev.currentTarget.select()}
-               value={value} type="number" onChange={(ev) => changeHandler(ev.currentTarget)}/>
+          onBlur={(ev) => setQuantity(card, type, ev.currentTarget)} onClick={(ev) => ev.currentTarget.select()}
+          value={value} type="number" onChange={(ev) => changeHandler(ev.currentTarget)} />
         <button className="CardCounter-plus" disabled={isDisabled}
-                onClick={() => modifyQuantity(card, type, 'plus')}>+
+          onClick={() => modifyQuantity(card, type, 'plus')}>+
         </button>
+        {((changeNotificationValue !== 0) && changeNotificationValue) &&
+          <div className={"CardCounter-diffNotif " + ((changeNotificationValue > 0) ? 'isPositive' : 'isNegative')}>{changeNotificationValue > 0 ? '+' : ''}{changeNotificationValue}</div>}
       </div>
     </div>
   )
